d89a1b767aff807a2457fed39f93b8a9998d1f24
Uviba LLC <ibrahim.hasanov.98@gmail.com>
2018-10-24 16:08:38 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+const uvipay = require('./uvipay');
+
+uvipay.setApiPrivateKey('sk_test_f45349e0dc5c6a7927fd75c19c00f448');
+uvipay.charge('0c66b09933685b7258d5bfa1971b_779', 1000).then(function (response) {
+    console.log('Charge (+)', response);
+    
+    uvipay.refund(response.id, 1000).then(function (response) {
+        console.log('Refund (+)',response);
+    }).catch(function(msg) {
+        console.log('Refund (-)',msg);
+    });
+
+}).catch(function(msg) {
+    console.log('Charge (-)', msg);
+});
