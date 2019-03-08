70efb22611732a40d6e47fec9b87ed11f8a67bc7
Uviba LLC <ibrahim.hasanov.98@gmail.com>
2019-03-07 20:58:07 -0800

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ uvipay.setApiPrivateKey('sk_test_f45349e0dc5c6a7927fd75c19c00f448');
 
 
 //To charge user
+//0c66b09933685b7258d5bfa1971b_779 is token came from iframe
 uvipay.charge('0c66b09933685b7258d5bfa1971b_779', 1000).then(function (response) {
     console.log('Charge (+)', response);
     //to refund user
@@ -18,10 +19,3 @@ uvipay.charge('0c66b09933685b7258d5bfa1971b_779', 1000).then(function (response)
 });
 
 
-//To send payments
-
-uvipay.send_payment({
-'amount':1000,
-'email':"example@example.com",
-'message': "Here is your payments",
-});
